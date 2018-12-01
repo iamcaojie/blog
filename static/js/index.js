47eb898ce3e6a9c4284cf2b453731efbb473d082
caojie“ <10804842@qq.com>
2018-12-01 16:22:28 +0800

    添加轮播图

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,52 @@
 //初始化layui
-layui.use(['layer', 'form'], function(){
+layui.use(['layer', 'form','carousel'], function(){
 var layer = layui.layer
-,form = layui.form;
-
+,form = layui.form
+,carousel = layui.carousel;
+  //轮播实例
+  carousel.render({
+    elem: '#carousel'
+    ,width: '100%' 
+    ,arrow: 'always' 
+    ,height: '456.34px'
+  });
 });
+//根据窗口获取轮播图高度
+// function carouselheight(){
+    // if ($(window).width() > ) {return '456.34px'};
+// }
+
+function IEVersion() {
+    //取得浏览器的userAgent字符串  
+    var userAgent = navigator.userAgent;
+    //判断是否IE<11浏览器  
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1;
+    //判断是否IE的Edge浏览器  
+    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE;
+    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    if(isIE) {
+        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+        reIE.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+            if(fIEVersion == 7) {
+                return 7;
+            } else if(fIEVersion == 8) {
+                return 8;
+            } else if(fIEVersion == 9) {
+                return 9;
+            } else if(fIEVersion == 10) {
+                return 10;
+            } else {
+                return 6;//IE版本<=7
+            }   
+    } else if(isEdge) {
+        return 'edge';//edge
+    } else if(isIE11) {
+        return 11; //IE11  
+    }else{
+        return -1;//不是ie浏览器
+    }
+}
 
 // jQuery特效
 $(function(){ 
@@ -14,12 +57,20 @@ $(function(){
     });
     // 检查搜索框
     $("#search").click(function(){
-        var search_text = $("#search_text").val()
+        var search_text = $("#search_text").val();
         if ( search_text == ""){
             layer.msg('请输入搜索内容！！！');
             return false;
         }
     });
+    
+    // $(window).resize(function(){
+        // if $(window).width()
+        // var box = $('#introduction').css('height');
+        // $('#carousel').css("height",);
+        // alert($('#introduction').css('height'));
+    // });
 });
 
 console.log("欢迎提交bug或建议，联系方式QQ10804842")
+// alert(IEVersion());
